9bd5cfa451786a6373cbcc8800e964c0f3e6db36
DiegoFourstage <diego.fourstage.github@gmail.com>
2019-12-01 14:21:47 -0300

    Estabelecendo Conexão

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.dal;
+
+import java.sql.*; //Conjunto de bibliotecas
+/**
+ *
+ * @author D1350
+ */
+public class ModuloConexao {
+    //Método responsavel por estabelecer conexão com o banco de dados
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        // Chamando o driver myql que foi importado na biblioteca
+        String driver = "com.mysql.jdbc.Driver";
+        // Armazenando informaçoes referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user="root";
+        String password="";
+        //Estabelecendo a conexão com o banco 
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch(Exception e){
+                        
+            System.out.println(e);
+            return null;
+        }
+        
+    }
+    
+}
--- src/br/com/infox/telas/TelaLogin.java
+++ src/br/com/infox/telas/TelaLogin.java
@@ -4,18 +4,26 @@
  * and open the template in the editor.
  */
 package br.com.infox.telas;
+import java.sql.*;
+import br.com.infox.dal.ModuloConexao;
 
 /**
  *
  * @author D1350
  */
 public class TelaLogin extends javax.swing.JFrame {
-
+    Connection conexao = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
+    
     /**
      * Creates new form TelaLogin
      */
     public TelaLogin() {
         initComponents();
+        conexao = ModuloConexao.conector();
+        
+        System.out.println(conexao);
     }
 
     /**
@@ -88,30 +96,9 @@ public class TelaLogin extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    
 
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new TelaLogin().setVisible(true);
